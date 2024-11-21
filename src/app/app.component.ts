1901d508e544f85aff03518f07b88e2129e366d9
Hugo7764 <martinezhu@cy-tech.fr>
2024-11-21 14:20:57 +0100

    home component html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { PublishComponent } from "./components/publish/publish.component";
 import { HomeComponent } from "./components/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PublishComponent, HomeComponent],
+  imports: [HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { PublishComponent } from './components/publish/publish.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'publish', component: PublishComponent },
 ];
 
 @NgModule({
