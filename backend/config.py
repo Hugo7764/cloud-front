f370329d5270f74a0e780d5585fd9479561664e7
Hugo7764 <martinezhu@cy-tech.fr>
2024-11-21 15:54:43 +0100

    .env & db credentials/URI

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,7 @@
+from dotenv import load_dotenv
+import os
+
 class Config:
-    SECRET_KEY = 'secret_key'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://zinedine:zidane@localhost/messagerie'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
