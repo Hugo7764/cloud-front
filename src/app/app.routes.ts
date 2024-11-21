2e29196baca3ae7c5a5723bc119b6b832b415198
Hugo7764 <martinezhu@cy-tech.fr>
2024-11-21 14:46:24 +0100

    ngModel problem fix, @NgModule config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'chat/:id', component: ChatComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavigationComponent,
+    ChatComponent,
+    LoginComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    RouterModule.forRoot(routes),
+    FormsModule, // Ajout de FormsModule ici
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
+
 export class AppRoutingModule { }
