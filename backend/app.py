5bafe823c0105fe1f44db4b735b34f57c01494a1
Hugo7764 <martinezhu@cy-tech.fr>
2024-11-28 14:52:29 +0100

    nettoyage d'imports

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask_cors import CORS
 from flask_mysql_connector import MySQL
-from flask_jwt_extended import create_access_token, jwt_required, JWTManager
-import bcrypt
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 CORS(app)
